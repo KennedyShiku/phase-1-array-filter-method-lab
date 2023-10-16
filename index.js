1e2e4339053b866a5dd265edd168c44063372994
KennedyShiku <kenrichshiku@gmail.com>
2023-10-16 07:44:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(driver => driver === name);
+}
+
+function findMatching(drivers, name){
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function findMatching(drivers, name){
+  const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  return matchingDrivers.length === 0 ? [] : matchingDrivers;
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
